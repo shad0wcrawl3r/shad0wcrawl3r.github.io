6a46bda761de96cfb2aed0c64b0babf43c575b48
Ashwin Belbase <46002059+shad0wcrawl3r@users.noreply.github.com>
2022-10-10 09:24:37 +0545

    Update dev.sh

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,7 +2,7 @@ sudo apt install -y zsh git build-essential curl
 sudo chsh $USER -s /bin/zsh
 bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -- --unattended
 bash -c "$(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs)" -- --profile complete --quiet -y && source "$HOME/.cargo/env" && bash -c "cargo install exa bat bottom hyperfine"
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/shad0wcrawl3r/shad0wcrawl3r.github.io/master/scripts/automate.sh)"
+sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/shad0wcrawl3r/shad0wcrawl3r.github.io/master/scripts/automate.sh)"
 mkdir -p $HOME/.config/ && cat > $HOME/.config/custom << EOF 
 alias cat='bat'
 alias ls='exa -l'
